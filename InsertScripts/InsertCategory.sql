fb7984cf2e74197489dd4324315db3a59b25ebf9
David Anicic <david.anicic369@gmail.com>
2020-08-13 15:25:49 +0200

    Add files via upload

---
+++ InsertScripts/InsertCategory.sql
@@ -0,0 +1,7 @@
+INSERT [dbo].[Categories] ( [Name], [Description]) VALUES (N'Mobile phones', N'Mobile phones description')
+INSERT [dbo].[Categories] ( [Name], [Description]) VALUES (N'TV', N'TV description')
+INSERT [dbo].[Categories] ( [Name], [Description]) VALUES ( N'Lap top', N'Lap top description')
+INSERT [dbo].[Categories] ([Name], [Description]) VALUES (N'Desktop', N'Desktop description')
+INSERT [dbo].[Categories] ( [Name], [Description]) VALUES ( N'Monitor', N'Monitor description')
+INSERT [dbo].[Categories] ( [Name], [Description]) VALUES ( N'Audio', N'Audio description')
+SET IDENTITY_INSERT [dbo].[Categories] ON 
